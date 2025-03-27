122a7947bb68b331253b445a05541c3c8230628d
Ryan O'Neill <ryanoneill@gmail.com>
2025-03-27 10:23:07 -0700

    History: Add `History`, `HistoryException`, and Tests
    This commit adds a `History` class, which is a back and forth
    conversation between a user and Claude. `UserMessage` and
    `ClaudeMessage` values should be interleaved, and trying to append the
    wrong type of `Message` at a point in the `History` will raise a
    `HistoryException`.

---
+++ src/throwpillow/history.py
@@ -0,0 +1,39 @@
+from throwpillow.message import Message, ClaudeMessage, UserMessage
+from typing import List
+
+class HistoryException(Exception):
+    def __init__(self, message: str):
+        self.message = message
+
+class History:
+    def __init__(self, items: List[Message] = []):
+        self.messages: List[Message] = []
+
+        for item in items:
+            self.append(item)
+
+    def append(self, message: Message) -> None:
+        n = len(self.messages)
+        is_user = n % 2 == 0
+
+        if is_user:
+            if isinstance(message, UserMessage):
+                self.messages.append(message)
+            else:
+                value = "It is the user's turn, and this is not a `UserMessage`"
+                raise HistoryException(value)
+        else:
+            if isinstance(message, ClaudeMessage):
+                self.messages.append(message)
+            else:
+                value = "It is Claude's turn, and this is not a `ClaudeMessage`"
+                raise HistoryException(value)
+
+    def __iter__(self):
+        return iter(self.messages)
+
+    def __len__(self) -> int:
+        return len(self.messages)
+
+    def __getitem__(self, index: int) -> Message:
+        return self.messages[index]
---
+++ tests/test_history.py
@@ -0,0 +1,69 @@
+import pytest
+from throwpillow.history import History, HistoryException
+from throwpillow.message import ClaudeMessage, UserMessage, Message
+from typing import List
+
+# Empty History Tests
+
+def test_history_creation():
+    history = History()
+    assert history is not None
+
+def test_history_is_empty():
+    history = History()
+    assert history.messages == []
+
+def test_history_has_0_len():
+    history = History()
+    assert len(history) == 0
+
+def test_history_iter_does_nothing():
+    history = History()
+    result = True
+    for _ in history:
+        result = False
+    assert result
+
+def test_history_get_item_fails():
+    history = History()
+    with pytest.raises(IndexError):
+        history[0]
+
+def test_history_append_works_for_user():
+    message = UserMessage("This should work")
+    history = History()
+    history.append(message)
+    assert True
+
+def test_history_append_fails_for_claude():
+    message = ClaudeMessage("This should not work")
+    history = History()
+    with pytest.raises(HistoryException):
+        history.append(message)
+
+# Initial User Message
+def test_history_creation_one_message_good():
+    message = UserMessage("This should work")
+    items: List[Message] = [message]
+    history = History(items)
+    assert len(history) == 1
+    
+def test_history_creation_one_message_bad():
+    message = ClaudeMessage("This should not work")
+    items: List[Message] = [message]
+    with pytest.raises(HistoryException):
+        History(items)
+
+def test_history_creation_two_messages_good():
+    message1 = UserMessage("This should work")
+    message2 = ClaudeMessage("So should this")
+    items: List[Message] = [message1, message2]
+    history = History(items)
+    assert len(history) == 2
+
+def test_history_creation_two_messages_bad():
+    message1 = UserMessage("This should work")
+    message2 = UserMessage("This should not")
+    items: List[Message] = [message1, message2]
+    with pytest.raises(HistoryException):
+        History(items)
