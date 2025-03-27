7736ba344a96cd65c4624b91c20bc35a9349de35
Ryan O'Neill <ryanoneill@gmail.com>
2025-03-26 19:08:26 -0700

    Client: Add the Beginnings of a Client
    This commit adds a rudimentary `Client` with a `send` method `str ->
    str`. It is intended for ease of use in sending single messages back
    and forth with Claude using the `anthropic` client.

---
+++ client.py
@@ -0,0 +1,24 @@
+from anthropic import Anthropic
+
+class Client:
+    def __init__(self):
+        # TODO: Be able to change the model fully
+        # TODO: Be able to change the max tokens fully
+        self.inner = Anthropic()
+        self.model = "claude-3-haiku-20240307"
+        self.max_tokens = 1000
+
+    def send(self, content: str) -> str:
+        # TODO: Be able to change the model per request
+        # TODO: Be able to change the max tokens per request
+        # TODO: Be able to make an asynchronous request
+        response = self.inner.messages.create(
+            model = self.model,
+            max_tokens = self.max_tokens,
+            messages = [
+                {"role": "user", "content": content}
+            ]
+        )
+        # TODO: Deal with errors
+        # TODO: Deal with different types of ContentBlocks.
+        return response.content[0].text
